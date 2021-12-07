7c0d18c62a8669b3c275a7dd81e220ab6f645b46
Data <95385516+thedataelemental@users.noreply.github.com>
2021-12-06 19:01:59 -0800

    Add files via upload

--- math_quiz.js
+++ math_quiz.js
@@ -3,28 +3,41 @@
 // Author: Jackie P, aka TheDataElemental
 
 
-let continue_choice = 'y'
+let continue_choice = 'y';
 
 // Game loop
 while (continue_choice == 'y') {
 	
-	// Generate random addition problem
+	// Generate numbers for random math problem
 	let first_number = Math.floor(Math.random() * 100);
 	let second_number = Math.floor(Math.random() * 100);
-	let sum = first_number + second_number;
-	player_answer = window.prompt(first_number + " + " + second_number + " = ?");
+	
+	// Flip a coin to determine if problem is addition or subtraction
+	let add_or_sub = Math.floor(Math.random() * 2);
+	
+	// Give player an addition problem
+	if (add_or_sub == 0) {	
+		var answer = first_number + second_number;
+		player_answer = window.prompt(first_number + " + " + second_number + " = ?");
+	}
+	
+	// Give player a subtraction problem
+	if (add_or_sub == 1) {	
+		var answer = first_number - second_number;
+		player_answer = window.prompt(first_number + " - " + second_number + " = ?");
+	}
 
 	// Tell player if they win or lose
-	if (player_answer == sum) {
+	if (player_answer == answer) {
 		alert("Correct! =D");
 	}
-	if (player_answer != sum) {
+	if (player_answer != answer) {
 		alert("Incorrect =(");
 	}
 	
 	// Ask player if they want to continue
-	continue_choice = ''
+	continue_choice = '';
 	while (!(['y', 'Y', 'n', 'N', 'yes', 'YES', 'no', 'NO'].includes(continue_choice))) {
-		continue_choice = window.prompt("Continue? y/n: ")
+		continue_choice = window.prompt("Continue? y/n: ");
 	}
 }
